cfa8a69481d990bf1b2d6a860015f7b9fdbdc8e3
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-20 18:10:47 +0200

    landing created and working correctly with buttons and links to main pages

--- scripts.js
+++ scripts.js
@@ -2,15 +2,19 @@ const playContainer = document.querySelector(".ps5-container");
 const xboxContainer = document.querySelector(".xbox-container");
 
 playContainer.addEventListener("mouseover", () => {
-    playContainer.style.width = '70%';
-    xboxContainer.style.width = '30%';
     playContainer.style.zIndex = 1;
     xboxContainer.style.zIndex = 0;
+    setTimeout(() => {
+        playContainer.style.width = '70%';
+        xboxContainer.style.width = '30%';
+    }, 100);
 })
 
 xboxContainer.addEventListener("mouseover", () => {
-    playContainer.style.width = '30%';
-    xboxContainer.style.width = '70%';
     playContainer.style.zIndex = 0;
     xboxContainer.style.zIndex = 1;
+    setTimeout(() => {
+        playContainer.style.width = '30%';
+        xboxContainer.style.width = '70%';
+    }, 100);
 })
