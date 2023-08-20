b2bab5b0203d65c84483b46e7fbfcdf83777a412
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-20 13:45:40 +0200

    transitions and zIndex working correctly just to improve buttons

--- scripts.js
+++ scripts.js
@@ -4,9 +4,13 @@ const xboxContainer = document.querySelector(".xbox-container");
 playContainer.addEventListener("mouseover", () => {
     playContainer.style.width = '70%';
     xboxContainer.style.width = '30%';
+    playContainer.style.zIndex = 1;
+    xboxContainer.style.zIndex = 0;
 })
 
 xboxContainer.addEventListener("mouseover", () => {
     playContainer.style.width = '30%';
     xboxContainer.style.width = '70%';
+    playContainer.style.zIndex = 0;
+    xboxContainer.style.zIndex = 1;
 })
